5cfe6bca56b728c696f2743b6bccbfe6f843fb68
Aysanat <aysanat21aysanat@gmail.com>
2024-12-07 11:05:26 +0300

    Выпадающее меню каталога при клике

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,8 @@
-console.log('script work!');
+$(document).ready(function() {
+
+  let $dropdown = $('.js-dropdown');
+
+  $('.js-menu-catalog').on('click', function() {
+    $dropdown.slideToggle();
+  })
+})
