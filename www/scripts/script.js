bac02bd5bdfccc8d25a6a62edff95210e961451b
Aysanat <aysanat21aysanat@gmail.com>
2024-12-20 20:14:56 +0300

    product(reviews, comment)

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -19,4 +19,70 @@ $(document).ready(function() {
 
   });
 
+  let prevAccordionBtn;
+  let openClass = 'open';
+
+
+  $('.js-accordion-btn').on('click', function() {
+    if (this === prevAccordionBtn) {
+      $(this).toggleClass(openClass)
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $(prevAccordionBtn).removeClass(openClass);
+    $(prevAccordionBtn).next().slideUp();
+
+    $(this).addClass(openClass)
+    $(this).next().slideDown();
+
+    prevAccordionBtn = this;
+  });
+
+
+
+
+  $('.js-reviews-more').on('click', function() {
+
+    let btn = this;
+
+    $(btn).attr('disabled', 'disabled')
+
+    $.ajax({
+      type: 'POST',
+      url: '../jsons/reviews.json',
+      data: {
+        quantity: 3
+      },
+
+      success: function(res) {
+        let reviewsHtmlString = createReviewsHtml(res.reviews)
+        console.log(reviewsHtmlString);
+
+        $('.js-reviews-list').append(reviewsHtmlString);
+        $(btn).removeAttr('disabled')
+      },
+      error: function() {
+        console.log('error');
+      }
+    });
+  });
+
+  function createReviewsHtml(reviewsArray) {
+    let result = ''
+
+    reviewsArray.forEach(function(review) {
+      result += `<div class="reviews_item">
+            <strong class="reviews_name">"${review.name}"</strong>
+            <p class="reviews_date">"${review.date}"</p>
+            <div class="reviews_stars">
+                  <i class="stars_icon">"${review.stars}"</i>
+                </div>
+            <p class="reviews_text">"${review.text}"</p>
+          </div>`
+    })
+
+    return result;
+  }
+
 })
